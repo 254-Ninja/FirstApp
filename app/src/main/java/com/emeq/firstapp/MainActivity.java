a133cd05c02722ee8c9d1c56230221afbfcc58e5
perfect-code-ninja <muthomidenis62@gmail.com>
2019-07-06 14:25:20 +0300

    Andela Basic

--- app/src/main/java/com/emeq/firstapp/MainActivity.java
+++ app/src/main/java/com/emeq/firstapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.emeq.firstapp;
 
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +22,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                TextView textView = findViewById(R.id.text_value);
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
